fb3161c5262b50ddbae95e10ee7a04786ab0599f
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-19 15:55:57 -0800

    adding account-mock

--- lab-pedja/__test__/lib/account-mock.js
+++ lab-pedja/__test__/lib/account-mock.js
@@ -1,1 +1,31 @@
 'use script';
+
+const faker = require('faker');
+const Account = require('../../model/account');
+
+const accountMock = module.exports = {};
+
+accountMock.create = () => {
+  let mock = {};
+  mock.request = {
+    username : faker.internet.userName(),
+    email : faker.internet.email(),
+    password : faker.lorem.words(5),
+  };
+
+  return Account.create(mock.request.username, mock.request.email, mock.request.password)
+    .then(account => {
+      mock.account = account;
+      return account.createToken();
+    })
+    .then(token => {
+      mock.token = token;
+      return Account.findById(mock.account._id);
+    })
+    .then(account => {
+      mock.account = account;
+      return mock;
+    });
+};
+
+accountMock.remove = () => Account.remove({});
