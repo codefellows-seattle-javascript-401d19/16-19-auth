3ad2d1bbe55111d186a93671ebbd9546477b1b3b
Jacob M-G Evans <jacobmgevans@gmail.com>
2017-12-20 15:17:20 -0800

    all the things

---
+++ lab-jacob/route/profile-router.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const {Router} = require('express');
+const jsonParser = require('body-parser');
+const httpErrors = require('http-errors');
+const Profile = require('../model/profile');
+
+const bearerAuthMiddleware = require('../lib/bearer-auth-middleware');
+
+const profileRouter = module.exports = new Router();
+
+profileRouter.post('/profiles', bearerAuthMiddleware,jsonParser, (request,reponse,next) => {
+  if(!request.account)
+    return next(new httpErrors(404, 'ERROR not found'));
+
+  return new Profile({
+    ...request.body,
+    account : request.account._id,
+  }).save()
+  .then(profile => response.json(profile))
+  .catch(next);
+});
