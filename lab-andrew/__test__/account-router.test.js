f12f2d14ed4d99aab1a9b4bd168f6d15dcefc51d
Andrew Bloom <blooma116@gmail.com>
2017-12-19 20:35:05 -0800

    setup files and first server test

---
+++ lab-andrew/__test__/account-router.test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+require('./lib/setup');
+const superagent = require('superagent');
+const server = require('../lib/server');
+const accountMock = require('./lib/account-mock');
+
+const __API_URL__ = `http://localhost:${process.env.PORT}/signup`;
+
+describe('AUTH Router', () => {
+  beforeAll(server.start);
+  afterAll(server.stop);
+  afterEach(accountMock.remove);
+
+  test('POST creating an account should respond with 200 and a token if there are no errors', () => {
+    return superagent.post(__API_URL__)
+      .send({
+        username : 'kitty',
+        email : 'kitty@cats.com',
+        password : 'catsrule',
+      })
+      .then(response => {
+        expect(response.status).toEqual(200);
+        expect(response.body.token).toBeTruthy();
+      });
+  });
+
+  test('POST /signup - an incomplete request should return a 400', () => {
+    return superagent.post(__API_URL__)
+      .send({
+        username: 'kitty',
+        email: 'kitty@cats.com',
+      })
+      // .then(Promise.reject())
+      .catch(response => {
+        console.log('hey');
+        expect(response.status).toEqual(400);
+      });
+  });
+});
---
+++ lab-andrew/__test__/lib/setup.js
@@ -0,0 +1,5 @@
+'use strict';
+
+process.env.PORT = 3000;
+process.env.MONGODB_URI = 'mongodb://localhost/testing';
+process.env.CAT_CLOUD_SECRET = 'supersecretcatpassword';
