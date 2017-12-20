0b785ce54d5efa6bd1c7dd030b35c580750c268a
Cameron Moorehead <hello@cameronmoorehead.com>
2017-12-19 16:18:27 -0800

    tests from class are passing:

---
+++ lab-cameron/__test__/auth-router.test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+require('./lib/setup');
+const superagent = require('superagent');
+const server = require('../lib/server');
+const accountMock = require('./lib/account-mock');
+
+const apiURL = `http://localhost:${process.env.PORT}/signup`;
+
+describe('auth-router.js', () => {
+  beforeAll(server.start);
+  afterAll(server.stop);
+  afterEach(accountMock.remove);
+
+  test('POST should create an account and respond with 200 and a token if there are no errors', () => {
+    return superagent.post(apiURL)
+      .send({
+        username: 'testname',
+        email: 'test@test.com',
+        password: 'secret',
+      })
+      .then(response => {
+        console.log(response.body);
+        expect(response.status).toEqual(200);
+        expect(response.body.token).toBeTruthy();
+      });
+  });
+
+  test('POST /signup - should return 400 if request body is incomplete', () => {
+    return superagent.post(apiURL)
+      .send({
+        username: 'test',
+        email: 'test@test.com',
+      })
+      .then(Promise.reject)
+      .catch(response => {
+        expect(response.status).toEqual(400);
+      });
+  });
+});
