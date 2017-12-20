118520268c2acb82d1416ae64640f876a8fb281d
David Lindahl <davidalindahl@gmail.com>
2017-12-19 20:05:12 -0800

    wrote out tests file

---
+++ lab-david/__test__/auth-router.test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+require('./lib/setup');
+
+const superagent = require('superagent');
+const server = require('../lib/server');
+const accountMock = require('./lib/account-mock');
+
+const apiURL = `http://localhost:${process.env.PORT}/signup`;
+
+describe('AUTH Router', () => {
+  beforeAll(server.start);
+  afterAll(server.stop);
+  afterEach(accountMock.remove);
+
+  test('POST creating an account should respond with a 200 and a token if there are no errors', () => {
+    return superagent.post(apiURL)
+      .send({
+        username : 'zaphod',
+        email : 'president@galaxy.org',
+        password : '42',
+      })
+      .then(response => {
+        console.log(response.body);
+        expect(response.status).toEqual(200);
+        expect(response.body.token).toBeTruthy();
+      });
+  });
+
+  test('POST /signup - an incomplete request should return a 400', () => {
+    return superagent.post(apiURL)
+      .send({
+        username : 'zaphod',
+        email : 'president@galaxy.org',
+      })
+      .then(Promise.reject)
+      .catch(response => {
+        expect(response.status).toEqual(400);
+      });
+  });
+});
