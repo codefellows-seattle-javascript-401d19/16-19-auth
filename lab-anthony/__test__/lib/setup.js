24a4d71720b4332a779b082b11512d4f0f0a9897
Anthony Robinson <robinsonamk@gmail.com>
2017-12-19 17:33:24 -0800

    finished tests

--- lab-anthony/__test__/lib/setup.js
+++ lab-anthony/__test__/lib/setup.js
@@ -2,4 +2,4 @@
 
 process.env.PORT = 7000;
 process.env.MONGODB_URI = 'mongodb://localhost/testing';
-process.env.SUPER_DUPER_SECRET = 'change_this';
+process.env.SUPER_DUPER_SECRET = 'stufffffffff';
