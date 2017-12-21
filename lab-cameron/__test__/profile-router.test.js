21eca770aeb9aa3f7a1c2e899d5993733ff2c716
Cameron Moorehead <hello@cameronmoorehead.com>
2017-12-20 17:45:35 -0800

    401 and 400 status tests on POST /profiles passing

--- lab-cameron/__test__/profile-router.test.js
+++ lab-cameron/__test__/profile-router.test.js
@@ -9,32 +9,67 @@ const profileMockFactory = require('./lib/profile-mock-factory');
 
 const apiURL = `http://localhost:${process.env.PORT}`;
 
-describe('POST /profiles', () => {
+describe('profile-router.js', () => {
   beforeAll(server.start);
   afterAll(server.stop);
   afterEach(profileMockFactory.remove);
 
-  test('Should return a 200 and a profile if there are no errors', () => {
-    let accountMock = null;
+  describe('POST /profiles', () => {
+    test('Should return a 200 and a profile if there are no errors', () => {
+      let accountMock = null;
 
-    return accountMockFactory.create()
-      .then(mock => {
-        accountMock = mock;
-        return superagent.post(`${apiURL}/profiles`)
-          .set('Authorization', `Bearer ${accountMock.token}`)
-          .send({
-            bio: 'test bio',
-            firstName: 'testFirst',
-            lastName: 'testLast',
-          });
-      })
-      .then(response => {
-        expect(response.status).toEqual(200);
-        expect(response.body.account).toEqual(accountMock.account._id.toString());
-        expect(response.body.firstName).toEqual('testFirst');
-        expect(response.body.lastName).toEqual('testLast');
-        expect(response.body.bio).toEqual('test bio');
-      })
-      .catch(error => console.log(error));
+      return accountMockFactory.create()
+        .then(mock => {
+          accountMock = mock;
+          return superagent.post(`${apiURL}/profiles`)
+            .set('Authorization', `Bearer ${accountMock.token}`)
+            .send({
+              bio: 'test bio',
+              firstName: 'testFirst',
+              lastName: 'testLast',
+            });
+        })
+        .then(response => {
+          expect(response.status).toEqual(200);
+          expect(response.body.account).toEqual(accountMock.account._id.toString());
+          expect(response.body.firstName).toEqual('testFirst');
+          expect(response.body.lastName).toEqual('testLast');
+          expect(response.body.bio).toEqual('test bio');
+        })
+        .catch(error => console.log(error));
+    });
+
+    test('should return a 400 status code if no auth header is provided', () => {
+      return accountMockFactory.create()
+        .then(() => {
+          return superagent.post(`${apiURL}/profiles`)
+            .send({
+              bio: 'test bio',
+              firstName: 'testFirst',
+              lastName: 'testLast',
+            });
+        })
+        .then(Promise.reject)
+        .catch(response => {
+          expect(response.status).toEqual(400);
+        });
+    });
+
+    test('should return a 401 status code if token is invalid or cannot be verified', () => {
+      return accountMockFactory.create()
+        .then(() => {
+          return superagent.post(`${apiURL}/profiles`)
+            .set('Authorization', 'Bearer invalidToken')
+            .send({
+              bio: 'test bio',
+              firstName: 'testFirst',
+              lastName: 'testLast',
+            });
+        })
+        .then(Promise.reject)
+        .catch(response => {
+          expect(response.status).toEqual(401);
+        });
+    });
   });
 });
