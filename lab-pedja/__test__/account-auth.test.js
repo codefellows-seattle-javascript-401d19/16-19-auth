44e2edf870fe815b4c753bad6d45dbf55e6b1295
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-20 19:28:58 -0800

    adding basic-auth-middleware and GET /login method and TEST

--- lab-pedja/__test__/account-auth.test.js
+++ lab-pedja/__test__/account-auth.test.js
@@ -7,49 +7,65 @@ const accountMock = require('./lib/account-mock');
 
 const apiURL = `http://localhost:${process.env.PORT}`;
 
-describe('AUTH Router', () => {
+describe('AUTH', () => {
   beforeAll(server.start);
   afterAll(server.stop);
   afterEach(accountMock.remove);
 
-  test('POST creating an account should responde with 200 and a token if no errors', () => {
-    return superagent.post(`${apiURL}/signup`)
-      .send({
+  describe('POST', () => {
+    test('POST creating an account should responde with 200 and a token if no errors', () => {
+      return superagent.post(`${apiURL}/signup`)
+        .send({
+          username : 'hound',
+          email : 'hound@hound.com',
+          password : 'supertopsecret',
+        })
+        .then(response => {
+          expect(response.status).toEqual(200);
+          expect(response.body.token).toBeTruthy();
+        });
+    });
+    test('POST sign up - incomplete request should return a 400 status code', () => {
+      return superagent.post(`${apiURL}/signup`)
+        .send({
+          username : 'hound',
+          email : 'hound@hound.com',
+        })
+        .then(Promise.reject)
+        .catch(response => {
+          expect(response.status).toEqual(400);
+        });
+    });
+    test('POST should return a 409 due to duplicate user property', () => {
+      let userToPost = {
         username : 'hound',
         email : 'hound@hound.com',
         password : 'supertopsecret',
-      })
-      .then(response => {
-        expect(response.status).toEqual(200);
-        expect(response.body.token).toBeTruthy();
-      });
+      };
+      return superagent.post(`${apiURL}/signup`)
+        .send(userToPost)
+        .then( () => {
+          return superagent.post(`${apiURL}/signup`)
+            .send(userToPost);
+        })
+        .then(Promise.reject)
+        .catch(response => {
+          expect(response.status).toEqual(409);
+        });
+    });
   });
-  test('POST sign up - incomplete request should return a 400 status code', () => {
-    return superagent.post(`${apiURL}/signup`)
-      .send({
-        username : 'hound',
-        email : 'hound@hound.com',
-      })
-      .then(Promise.reject)
-      .catch(response => {
-        expect(response.status).toEqual(400);
-      });
-  });
-  test('POST should return a 409 due to duplicate user property', () => {
-    let userToPost = {
-      username : 'hound',
-      email : 'hound@hound.com',
-      password : 'supertopsecret',
-    };
-    return superagent.post(`${apiURL}/signup`)
-      .send(userToPost)
-      .then( () => {
-        return superagent.post(`${apiURL}/signup`)
-          .send(userToPost);
-      })
-      .then(Promise.reject)
-      .catch(response => {
-        expect(response.status).toEqual(409);
-      });
-  });
+  describe('GET /login', () => {
+    test('GET /login should get a 200 status code and token if no errors occur', () => {
+      return accountMock.create()
+        .then(mock => {
+          return superagent.get(`${apiURL}/login`)
+            .auth(mock.request.username,mock.request.password);
+        })
+        .then(response => {
+          console.log(response.body);
+          expect(response.status).toEqual(200);
+          expect(response.body.token).toBeTruthy();
+        });
+    });
+  }); 
 });
