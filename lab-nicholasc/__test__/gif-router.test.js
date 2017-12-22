29d8fe95200b0edf093ead98d8c08d6113815982
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-12-21 17:49:57 -0800

    get by id test green

--- lab-nicholasc/__test__/gif-router.test.js
+++ lab-nicholasc/__test__/gif-router.test.js
@@ -32,4 +32,19 @@ describe('/gifs', () => {
           });
       });
   });
+  test('GET should return 200 and gif with id if no errors', () => {
+    let gifToTest = null;
+    return gifMockFactory.create()
+      .then(gif => {
+        gifToTest = gif;
+        return superagent.get(`${apiUrl}/gifs/${gif.gif._id}`)
+          .set('Authorization', `Bearer ${gif.accountMock.token}`);
+      })
+      .then(response => {
+        expect(response.status).toEqual(200);
+        console.log(response.body);
+        expect(response.body.title).toEqual(gifToTest.gif.title);
+        expect((response.body._id).toString()).toEqual((gifToTest.gif._id).toString());
+      });
+  });
 });
