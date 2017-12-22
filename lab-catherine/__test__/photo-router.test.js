26abec8575344b9c6ba4c40f5761e34ccd8e518b
Catherine Looper <loopercat@gmail.com>
2017-12-21 17:09:12 -0800

    Added a test for 401 POST errors

--- lab-catherine/__test__/photo-router.test.js
+++ lab-catherine/__test__/photo-router.test.js
@@ -32,4 +32,40 @@ describe('/sounds', () => {
           });
       });
   });
+
+  test('POST /photos should return a 400 status code if invalid request', () => {
+    let tempAccountMock = null;
+
+    return accountMockFactory.create()
+      .then(accountMock => {
+        tempAccountMock = accountMock;
+
+        return superagent.post(`${apiURL}/photos`)
+          .set('Authorization', `Bearer ${accountMock.token}`)
+          .field('invalidField', 'cat photo')
+          .attach('photo', `${__dirname}/asset/mooshy.jpg`)
+          .then(Promise.reject)
+          .catch(response => {
+            expect(response.status).toEqual(400);
+          });
+      });
+  });
+
+  test('POST /photos should return a 401 status code if invalid token', () => {
+    let tempAccountMock = null;
+
+    return accountMockFactory.create()
+      .then(accountMock => {
+        tempAccountMock = accountMock;
+
+        return superagent.post(`${apiURL}/photos`)
+          .set('Authorization', `Bearer is invalid`)
+          .field('title', 'cat photo')
+          .attach('photo', `${__dirname}/asset/mooshy.jpg`)
+          .then(Promise.reject)
+          .catch(response => {
+            expect(response.status).toEqual(401);
+          });
+      });
+  });
 });
