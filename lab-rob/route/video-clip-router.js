a0123ae1b86f65c5c374e2d3fa83ec6e935834a0
Robert Reed <robert.mc.reed@gmail.com>
2017-12-21 17:35:17 -0800

    last commit before setup

---
+++ lab-rob/route/video-clip-router.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const {Router} = require('express');
+const httpErrors = require('http-errors');
+const bearerAuth = require('../lib/bearer-auth-middleware');
+const VideoClip = require('../model/video-clip');
+const multer = require('multer');
+const upload = multer({dest: `${__dirname}/../temp`});
+const s3 = require('../lib/s3');
+
+const videoClipRouter = module.exports = new Router();
+
+videoClipRouter.post('/video-clips', bearerAuth, upload.any(), (request, response, next) => {
+  if(!request.account)
+    return next(new httpErrors(404, '__ERROR__ not found'));
+  
+  if(!request.body.title || request.files.length > 1 || request.files[0].fieldname !== 'video-clip')
+    return next(new httpErrors(400, '__ERROR__ invalid request'));
+
+  let file = request.files[0];
+  let key = `${file.filename}.${file.originalname}`;
+
+  return s3.upload(file.path, key)
+    .then(url => {
+      let videoClipData = {
+        title: request.body.title,
+        account: request.account._id,
+        url,
+      };
+      
+      if(request.body.duration)
+        videoClipData.duration = request.body.duration;
+
+      if(request.body.location)
+        videoClipData.location = request.body.location;
+
+      return new VideoClip(videoClipData).save();
+    })
+    .then(videoClip => response.json(videoClip))
+    .catch(next);
+});
